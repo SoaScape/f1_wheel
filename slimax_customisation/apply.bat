e80f7ca9d287d3e42117ace21ba438bde7107ed9
tp23838 <michael.trenaman@lv.com>
2016-07-16 10:47:17 +0100

    Added script to copy files into slimax directory

---
+++ slimax_customisation/apply.bat
@@ -0,0 +1,11 @@
+@echo off
+SET location="c:\Program Files (x86)\SLIMax Manager"
+echo Copying into %location%.
+echo.
+copy /Y cfg\* %location%\cfg
+copy /Y scripts\* %location%\scripts
+echo.
+echo Done.
+echo.
+echo.
+pause
