9b19b3d8cda19a56e51bbc9744aa5aeb0ad09db9
tp23838 <michael.trenaman@lv.com>
2016-08-03 12:06:04 +0100

    Refactored and simplified the getOpenMenuButtons function

--- slimax_customisation/scripts/mikes_custom_plugins/mike_codemasters_f1_utils.lua
+++ slimax_customisation/scripts/mikes_custom_plugins/mike_codemasters_f1_utils.lua
@@ -2,15 +2,14 @@ function getOpenMenuButtons(chosenMenu)
 	local buttons = {}
 	local currentMenu = buttonTrackerMap[quickMenuToggleButton] % (numMenus + 1)
 	if currentMenu ~= chosenMenu then
-		local closeMenuClicks = 0
 		if chosenMenu < currentMenu then
-			closeMenuClicks = (numMenus + 1) - currentMenu
+			local closeMenuClicks = (numMenus + 1) - currentMenu
 			for i = 0, closeMenuClicks + chosenMenu - 1 do
 				buttons[i] = quickMenuToggleKey
 				buttonTrackerMap[quickMenuToggleButton] = buttonTrackerMap[quickMenuToggleButton] + 1
 			end
 		else
-			for i = 0, chosenMenu - currentMenu - 1 + closeMenuClicks do
+			for i = 0, chosenMenu - currentMenu - 1 do
 				buttons[i] = quickMenuToggleKey
 				buttonTrackerMap[quickMenuToggleButton] = buttonTrackerMap[quickMenuToggleButton] + 1
 			end
