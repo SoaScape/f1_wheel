a07286ddc98fedbf0e80c9e3f9b4021c62a84d1b
tp23838 <michael.trenaman@lv.com>
2016-08-20 10:54:30 +0100

    Updated apply script locations and f1 2016 OSP

--- slimax_customisation/apply.bat
+++ slimax_customisation/apply.bat
@@ -1,9 +1,11 @@
 @echo off
-SET location="c:\Program Files (x86)\SLIMax Manager Pro"
-echo Copying into %location%.
+SET script_location="c:\Program Files (x86)\SLIMax Manager Pro\scripts"
+SET cfg_location="M:\Documents\My Documents\SLIMax Manager Pro\cfg"
+
+echo Copying into %script_location%.
 echo.
-xcopy /E /Y cfg\* %location%\cfg
-xcopy /E /Y scripts\* %location%\scripts
+xcopy /E /Y cfg\* %cfg_location%
+xcopy /E /Y scripts\* %script_location%
 echo.
 echo Done.
 echo.
