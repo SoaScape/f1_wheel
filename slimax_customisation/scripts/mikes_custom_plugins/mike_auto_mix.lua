dce31ffc27779dcef1ec97920ccab287249aea95
tp23838 <mike.trenaman@gmail.com>
2016-12-03 22:20:28 +0000

    Various automix improvements

--- slimax_customisation/scripts/mikes_custom_plugins/mike_auto_mix.lua
+++ slimax_customisation/scripts/mikes_custom_plugins/mike_auto_mix.lua
@@ -9,25 +9,50 @@ autoMixLearnFullThrottleStartTicks = 0
 autoMixLearnFullThrottleStartDistance = 0
 autoMixLearnFullThrottleActive = false
 
+autoMixLearnLowThrottleTimeout = 4000
+autoMixLearnLowThrottleStartTicks = 0
+autoMixLearnLowThrottleStartDistance = 0
+autoMixLearnLowThrottleActive = false
+
 function resetAutoMixData()
 	autoMixLearnedData = {}
 	autoMixLearnFullThrottleActive = false
+	autoMixLearnLowThrottleActive = false
 end
 
 function autoMixRegularProcessing()
 	if autoMixEnabled and mSessionEnter == 1 and not(m_is_sim_idle) then		
 		learnTrack()		
-		if currentMultifunction ~= nil and currentMultifunction["name"] == autoMixMultifunctionName
-		  then
-			local distance = GetContextInfo("lap_distance")
-			local autoMixEvent = autoMixLearnedData[round(distance, 1)]
-			if autoMixEvent ~= nil then
-				local fuelMode = autoMixEvent					
-				multiFunctionBak = currentMultifunction
-				currentMultifunction = fuelMultiFunction					
-				currentMultifunction["currentUpDnMode"] = fuelMode
-				confirmSelection("AUTO", fuelMultiFunction["modes"][fuelMode], myDevice, getButtonMap(currentMultifunction), showDisplay)					
-				currentMultifunction = multiFunctionBak
+		if currentMultifunction ~= nil and currentMultifunction["name"] == autoMixMultifunctionName then
+			if autoMixActiveType == nil then -- Automix not currently set, check if we can set it
+				local distance = GetContextInfo("lap_distance")
+				activeAutoMixData = autoMixLearnedData[round(distance, 0)]				
+				if activeAutoMixData ~= nil then
+					local autoMix = activeAutoMixData["mix"]	
+					autoMixReturnMix = activeAutoMixData["returnMix"]
+					fuelMultiFunction["currentUpDnMode"] = autoMix
+					confirmSelection("AUTO", fuelMultiFunction["modes"][autoMix], myDevice, getButtonMap(fuelMultiFunction), true)
+					if autoMix == fuelMultiFunction["max"] then
+						autoMixActiveType = "max"
+					else
+						autoMixActiveType = "min"
+					end
+				end
+			elseif autoMixActiveType ~= nil then
+				local throttle = GetCarInfo("throttle")
+				if autoMixActiveType == "max" then
+					if throttle < 1 then
+						autoMixActiveType = nil
+						fuelMultiFunction["currentUpDnMode"] = autoMixReturnMix
+						confirmSelection("AUTO", autoMixReturnMix, myDevice, getButtonMap(fuelMultiFunction), true)
+					end
+				else
+					if throttle == 1 then
+						autoMixActiveType = nil
+						fuelMultiFunction["currentUpDnMode"] = autoMixReturnMix
+						confirmSelection("AUTO", autoMixReturnMix, myDevice, getButtonMap(fuelMultiFunction), true)
+					end
+				end				
 			end
 		end
 	end
@@ -36,19 +61,55 @@ end
 function learnTrack()
 	local throttle = GetCarInfo("throttle")
 
-	if autoMixLearnFullThrottleActive and throttle < 1 then
-		autoMixLearnFullThrottleActive = false
-		
-		if (getTks() - autoMixLearnFullThrottleStartTicks) >= autoMixLearnFullThrottleTimeout then
-			local distance = GetContextInfo("lap_distance")
-			autoMixLearnedData[round(distance, 1)] = fuelMultiFunction["defaultUpDnMode"]
-			autoMixLearnedData[round(autoMixLearnFullThrottleStartDistance, 1)] = fuelMultiFunction["max"]
-			--activatePermanentLed(pattern, 500, false)
-			display("AMIX", distance, myDevice, 500)				
+	if autoMixLearnFullThrottleActive then
+		if throttle < 1 then
+			autoMixLearnFullThrottleActive = false
+			if highLearnt then
+				lastHighTime = getTks()
+			end
+		elseif (getTks() - autoMixLearnFullThrottleStartTicks) >= autoMixLearnFullThrottleTimeout and not (highLearnt) then				
+			highLearnt = true
+			if lastLowTime ~= nil and lastLowTime + 1000 > autoMixLearnFullThrottleStartTicks then
+				lastLowMixEvent["returnMix"] = fuelMultiFunction["max"]
+				display("ZMIX", fuelMultiFunction["modes"][fuelMultiFunction["max"]], myDevice, 500)
+			else
+				autoMixLearnedData[autoMixLearnFullThrottleStartDistance] = {}
+				autoMixLearnedData[autoMixLearnFullThrottleStartDistance]["mix"] = fuelMultiFunction["max"]
+				autoMixLearnedData[autoMixLearnFullThrottleStartDistance]["returnMix"] = fuelMultiFunction["defaultUpDnMode"]
+				lastHighMixEvent = autoMixLearnedData[autoMixLearnFullThrottleStartDistance]
+				display("AMIX", fuelMultiFunction["modes"][fuelMultiFunction["max"]], myDevice, 500)
+			end			
 		end
-	elseif throttle == 1 and not (autoMixLearnFullThrottleActive) then
+	elseif throttle == 1 then
 		autoMixLearnFullThrottleStartTicks = getTks()
-		autoMixLearnFullThrottleStartDistance = GetContextInfo("lap_distance")
+		autoMixLearnFullThrottleStartDistance = round(GetContextInfo("lap_distance"), 0)
 		autoMixLearnFullThrottleActive = true
+		highLearnt = false
+	end
+	
+	if autoMixLearnLowThrottleActive then
+		if throttle == 1 then
+			autoMixLearnLowThrottleActive = false
+			if lowLearnt then
+				lastLowTime = getTks()
+			end
+		elseif (getTks() - autoMixLearnLowThrottleStartTicks) >= autoMixLearnLowThrottleTimeout and not (lowLearnt) then
+			lowLearnt = true
+			if lastHighTime ~= nil and lastHighTime + 1000 > autoMixLearnLowThrottleStartTicks then
+				lastHighMixEvent["returnMix"] = fuelMultiFunction["min"]
+				display("ZMIX", fuelMultiFunction["modes"][fuelMultiFunction["min"]], myDevice, 500)
+			else
+				autoMixLearnedData[autoMixLearnLowThrottleStartDistance] = {}
+				autoMixLearnedData[autoMixLearnLowThrottleStartDistance]["mix"] = fuelMultiFunction["min"]
+				autoMixLearnedData[autoMixLearnLowThrottleStartDistance]["returnMix"] = fuelMultiFunction["defaultUpDnMode"]
+				lastLowMixEvent = autoMixLearnedData[autoMixLearnLowThrottleStartDistance]			
+				display("AMIX", fuelMultiFunction["modes"][fuelMultiFunction["min"]], myDevice, 500)
+			end
+		end
+	elseif throttle < 1 then
+		autoMixLearnLowThrottleStartTicks = getTks()
+		autoMixLearnLowThrottleStartDistance = round(GetContextInfo("lap_distance"), 0)
+		autoMixLearnLowThrottleActive = true
+		lowLearnt = false
 	end
 end
