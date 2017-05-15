1d8926298c462025f15d8c2ec62562abf2126421
tp23838 <michael.trenaman@gmail.com>
2017-05-15 21:57:30 +0100

    Copy diff maps when running apply.bat

--- slimax_customisation/apply.bat
+++ slimax_customisation/apply.bat
@@ -1,11 +1,13 @@
 @echo off
 SET script_location="C:\Program Files (x86)\SLIMax Manager Pro\scripts"
 SET cfg_location="M:\Documents\My Documents\SLIMax Manager Pro\cfg"
+SET diff_location="C:\Program Files (x86)\SLIMax Manager Pro\diff-maps"
 
 echo Copying into %script_location%.
 echo.
 xcopy /E /Y cfg\* %cfg_location%
 xcopy /E /Y scripts\* %script_location%
+xcopy /E /Y diff-maps\* %diff_location%
 del %script_location%\testharness.lua
 echo.
 echo Done.
