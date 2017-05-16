30f1495168a13c75253e922052ec7bb22f227d07
tp23838 <michael.trenaman@gmail.com>
2017-05-16 16:44:36 +0100

    Set track multifunction to 9

--- slimax_customisation/scripts/simracef1_f1_2016_mike.lua
+++ slimax_customisation/scripts/simracef1_f1_2016_mike.lua
@@ -193,7 +193,7 @@ multifunctionMap[12]["display"] = false
 -- Used by the overtake button
 fuelMultiFunction = multifunctionMap[2]
 diffMultiFunction = multifunctionMap[3]
-trackMultiFunction = multifunctionMap[10]
+trackMultiFunction = multifunctionMap[9]
 overtakeButtonEnabled = true
 overtakeOspOverdrive = false
 autoMixEnabled = true
