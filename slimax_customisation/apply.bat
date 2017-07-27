e74c89122a403d9019eaf6a6a89644b65016765f
tp23838 <michael.trenaman@gmail.com>
2017-07-27 21:42:12 +0100

    Updated apply script to use Program Files

--- slimax_customisation/apply.bat
+++ slimax_customisation/apply.bat
@@ -1,13 +1,13 @@
 @echo off
-SET script_location="C:\Program Files (x86)\SLIMax Manager Pro\scripts"
+SET script_location="C:\Program Files\SLIMax Manager Pro\scripts"
 SET cfg_location="M:\Documents\My Documents\SLIMax Manager Pro\cfg"
-SET diff_location="C:\Program Files (x86)\SLIMax Manager Pro\auto-cfg"
+SET autocfg_location="C:\Program Files\SLIMax Manager Pro\auto-cfg"
 
 echo Copying into %script_location%.
 echo.
 xcopy /E /Y cfg\* %cfg_location%
 xcopy /E /Y scripts\* %script_location%
-xcopy /E /Y auto-cfg\* %diff_location%
+xcopy /E /Y auto-cfg\* %autocfg_location%
 del %script_location%\testharness.lua
 echo.
 echo Done.
