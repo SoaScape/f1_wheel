45bb6ae85f07179755cb337be29b833b915b99d2
Mike Trenaman <mike.trenaman@gmail.com>
2018-07-05 21:36:50 +0100

    More f1 2018 keystroke delay changes

--- slimax_customisation/scripts/vdashemu_f1_2018_mercedes_mike.lua
+++ slimax_customisation/scripts/vdashemu_f1_2018_mercedes_mike.lua
@@ -299,18 +299,18 @@ raceStartModeEnabled = true
 safetyCarModeEnabled = true
 
 customKeystrokeDelays = {}
-customKeystrokeDelays[quickMenuLeft] = 60
-customKeystrokeDelays[quickMenuRight] = 60
-customKeystrokeDelays[quickMenuUp] = 60
-customKeystrokeDelays[quickMenuDn] = 60
-customKeystrokeDelays[fuelMultiFunction["incrementKey"]] = 60
-customKeystrokeDelays[fuelMultiFunction["decrementKey"]] = 60
-customKeystrokeDelays[diffMultiFunction["incrementKey"]] = 60
-customKeystrokeDelays[diffMultiFunction["decrementKey"]] = 60
-customKeystrokeDelays[biasMultiFunction["incrementKey"]] = 60
-customKeystrokeDelays[biasMultiFunction["decrementKey"]] = 60
-customKeystrokeDelays[ersMultiFunction["incrementKey"]] = 60
-customKeystrokeDelays[ersMultiFunction["decrementKey"]] = 60
+customKeystrokeDelays[quickMenuLeft] = 80
+customKeystrokeDelays[quickMenuRight] = 80
+customKeystrokeDelays[quickMenuUp] = 80
+customKeystrokeDelays[quickMenuDn] = 80
+customKeystrokeDelays[fuelMultiFunction["incrementKey"]] = 80
+customKeystrokeDelays[fuelMultiFunction["decrementKey"]] = 80
+customKeystrokeDelays[diffMultiFunction["incrementKey"]] = 80
+customKeystrokeDelays[diffMultiFunction["decrementKey"]] = 80
+customKeystrokeDelays[biasMultiFunction["incrementKey"]] = 80
+customKeystrokeDelays[biasMultiFunction["decrementKey"]] = 80
+customKeystrokeDelays[ersMultiFunction["incrementKey"]] = 80
+customKeystrokeDelays[ersMultiFunction["decrementKey"]] = 80
 
 function custom_init_Event(scriptfile)
 end
